1aa5377381dadfebdce5d949732df88a5fc4bae7
Arkadiusz Adamski <arkadiusz.adamski@allegro.pl>
2018-11-20 15:10:48 +0100

    WIP

--- src/ralph/settings/hooks.py
+++ src/ralph/settings/hooks.py
@@ -3,7 +3,10 @@ import os
 from ralph.lib.hooks import hook_name_to_env_name
 
 
-HOOKS = ('back_office.transition_action.email_context',)
+HOOKS = (
+    'back_office.transition_action.email_context',
+    'account.views.get_asset_list_class',
+)
 HOOKS_CONFIGURATION = {
     hook: os.environ.get(hook_name_to_env_name(hook), 'default')
     for hook in HOOKS
